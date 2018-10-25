51841cb1381f0c3ac00ed518326874df564b286a
Ankit Purohit - Network <purohit_ankit@network.lilly.com>
2018-10-25 12:12:26 +0530

    Updated py file

--- TriggerPPTApp.py
+++ TriggerPPTApp.py
@@ -1,23 +1,9 @@
-
-
-import sys
-import os
-import shutil
-import re
-import glob
-
-
-sftp_in = 'C:\Projects\Projects\PowerApps Project\Python\PPTMerge\In'
-sftp_working = 'C:\Projects\Projects\PowerApps Project\Python\PPTMerge\working'
-sftp_out = 'C:\Projects\Projects\PowerApps Project\Python\PPTMerge\out'
-sftp_dataFile = 'mergeData.json'
-
-
 from flask import Flask
 app = Flask(__name__)
-@app.route("/")
-def main():
-  return "Welcome!"
 
-if __name__ == "__main__":
-  app.run()
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run()
